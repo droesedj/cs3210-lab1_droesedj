073ad69ec0adc8db6d172f3ef483b12e876ce971
Dennis Droese <droesedj@msoe.edu>
2018-03-07 10:24:25 -0600

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+/**=============================================
+ * CS3210
+ * LAB 1: STL FILE READER
+ * @file main.cpp
+ * @date March 7 2018
+ * @author Dennis Droese
+ =============================================*/
+
+#include<iostream>
+
+
+int main(){
+	std::cout << "Hello there!";
+}
